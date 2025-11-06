c57cd5fb41999ba93b2ba395b2b7b250ada417b8
lenliu3 <46852347+lenliu3@users.noreply.github.com>
2025-11-06 12:13:50 -0800

    Remove key mappings for LSP functions
    Removed key mappings for 'gd', 'gr', and 'gi' in LSP.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -14,11 +14,8 @@ return {
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
           local opts = { buffer = args.buf }
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
           vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
           vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
         end,
       })
       
