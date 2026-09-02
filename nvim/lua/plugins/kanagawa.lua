5c63f7bec11ee083f88f65c564b7678648f951ce
Allen Liu <alliucaneat@gmail.com>
2026-09-02 11:42:10 -0700

    enable transparency for kanagawa

--- nvim/lua/plugins/kanagawa.lua
+++ nvim/lua/plugins/kanagawa.lua
@@ -3,7 +3,9 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    require("kanagawa").setup()
+    require("kanagawa").setup({
+      transparent = true,
+    })
     -- Variants: kanagawa-wave (dark, active), kanagawa-dragon (darkest), kanagawa-lotus (light).
     vim.cmd("colorscheme kanagawa-wave")
   end,
